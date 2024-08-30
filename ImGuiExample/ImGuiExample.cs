1e39dd288749d893e693c629366bc7fa365e8636
art0007i <art0007i@gmail.com>
2024-08-30 20:13:47 +0200

    Add project files.

---
+++ ImGuiExample/ImGuiExample.cs
@@ -0,0 +1,48 @@
+using ResoniteModLoader;
+using ImGuiUnityInject;
+using ImGuiNET;
+using FrooxEngine;
+using Elements.Core;
+using UnityEngine.SceneManagement;
+using System.Linq;
+using UnityFrooxEngineRunner;
+
+namespace ImGuiExample;
+
+public class ImGuiExample : ResoniteMod
+{
+    public override string Name => "ImGuiExample";
+    public override string Author => "art0007i";
+    public override string Version => "1.0.0";
+    public override string Link => "https://github.com/art0007i/ImGuiExample/";
+    public override void OnEngineInit()
+    {
+        string msg = "Hello World";
+        bool show = true;
+        ImGuiInstance.GetOrCreate((gui, isNew) =>
+        {
+            if (!isNew) return;
+            gui._camera = SceneManager.GetActiveScene().GetRootGameObjects().Where(go => go.name == "FrooxEngine").First().GetComponent<FrooxEngineRunner>().OverlayCamera;
+            gui.enabled = true;
+        }).Layout += () =>
+        {
+            ImGui.Begin("ImGuiExample Window");
+            ImGui.InputText("Message Text", ref msg, 1000);
+            ImGui.Checkbox("Show to Others", ref show);
+            if (ImGui.Button("Show message"))
+            {
+                var world = show ? Engine.Current.WorldManager.FocusedWorld : Userspace.UserspaceWorld;
+                var localUserRoot = world.LocalUser.Root;
+                if (world != null && localUserRoot != null)
+                    NotificationMessage.SpawnTextMessage(world,
+                        localUserRoot.ViewPosition + (localUserRoot.ViewRotation * float3.Forward * 0.25f * localUserRoot.GlobalScale),
+                        msg,
+                        RandomX.Hue,
+                        size: 0.35f*localUserRoot.GlobalScale,
+                        floatUpDistance: 0.05f * localUserRoot.GlobalScale);
+
+            }
+            ImGui.End();
+        };
+    }
+}
---
+++ README.md
@@ -0,0 +1,8 @@
+# ImGuiExample
+
+A [ResoniteModLoader](https://github.com/resonite-modding-group/ResoniteModLoader) mod for [Resonite](https://resonite.com/) that creates a simple ImGui window using [ImGuiUnityInject](https://github.com/art0007i/ImGuiUnityInject).
+
+## Installation
+1. Install [ResoniteModLoader](https://github.com/resonite-modding-group/ResoniteModLoader).
+1. Place [ImGuiExample.dll](https://github.com/art0007i/ImGuiExample/releases/latest/download/ImGuiExample.dll) into your `rml_mods` folder. This folder should be at `C:\Program Files (x86)\Steam\steamapps\common\Resonite\rml_mods` for a default install. You can create it if it's missing, or if you launch the game once with ResoniteModLoader installed it will create the folder for you.
+1. Start the game. If you want to verify that the mod is working you can check your Resonite logs.
